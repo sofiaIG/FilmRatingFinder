33e3150ed2eae5dbc585f16b250338ac6f1130a5
sofiaIG <sphignatiadi@gmail.com>
2021-11-06 16:19:55 +0000

    argparse is imported

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
+import argparse
 import imdb
 import rotten_tomatoes_client
+import argparse
+import sys
+
+
 moviesDB_IMDb = imdb.IMDb()
 moviesDB_rotten = rotten_tomatoes_client.RottenTomatoesClient
 
-def user_input():
-    user = input("Enter tha name of the film: ")
-    year_release = int(input("Enter the year of release: "))
-    values = [user, year_release]
-    return values
+my_parser = argparse.ArgumentParser(description="Name of the film and date of release")
+
+my_parser.add_argument("Film",
+                       metavar="film",
+                       type=str,
+                       help="the film title")
+
+my_parser.add_argument("Date",
+                       metavar="date",
+                       type=int,
+                       help="release date of the film")
+
+args = my_parser.parse_args()
+
+input_title = args.Film
+input_date = args.Date
+
 
 def search_score_rotten(user, year_release):
     movie_search = moviesDB_rotten.search(user)
@@ -19,13 +36,13 @@ def search_score_rotten(user, year_release):
             return movie["meterScore"]
 
 
-
 def find_movie_imdb(year_of_release, user):
     movie_search = moviesDB_IMDb.search_movie(user)
     for movie in movie_search:
         year = movie['year']
         if int(year) == year_of_release:
             return movie
+
 
 def search_filmratings_imdb(year_release, user):
     movie_found = find_movie_imdb(year_release, user)
@@ -35,11 +52,9 @@ def search_filmratings_imdb(year_release, user):
     return rating
 
 
-values = user_input()
-user = values[0]
-year_release = values[1]
-score_rotten = search_score_rotten(user, year_release)
-score_imdb = search_filmratings_imdb(year_release, user)
-print(f"The film {user} is rated {score_imdb} on IMDB and {score_rotten} on rotten tomatoes.")
-
-
+score_rotten = search_score_rotten(input_title, input_date)
+score_imdb = search_filmratings_imdb(input_date, input_title)
+if score_rotten == None and score_imdb == None:
+    print("Film not found")
+    sys.exit()
+print(f"The film {input_title} is rated {score_imdb} on IMDB and {score_rotten} on rotten tomatoes.")
